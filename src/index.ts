4d08a467580f8016454cea27c12542a03e6a0d43
seonghyeok0529 <seonghyeok0529@gmail.com>
2026-02-28 23:29:06 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"; 
 
 dotenv.config();
 
